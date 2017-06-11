e881a20166828fed4cae52d51b41cbaa72ae6da1
Richard Feldman <richard.t.feldman@gmail.com>
2017-06-11 19:14:04 +0200

    Update for 6.1.2

---
+++ tests/PhotoGrooveTests.elm
@@ -0,0 +1,10 @@
+module PhotoGrooveTests exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, list, int, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
