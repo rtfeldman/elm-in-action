39434c2b586bd99b550be9a2407cee0eda5570bc
Richard Feldman <richard.t.feldman@gmail.com>
2017-06-11 20:09:22 +0200

    Update for 6.2.2

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -4,14 +4,18 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, list, int, string)
 import Test exposing (..)
 import PhotoGroove exposing (..)
-import Json.Decode exposing (decodeString)
+import Json.Decode exposing (decodeValue)
+import Json.Encode as Encode
 
 
 decoderTest : Test
 decoderTest =
-    test "title defaults to (untitled)" <|
-        \_ ->
-            """{"url": "foo.com", "size": 5}"""
-                |> decodeString photoDecoder
+    fuzz2 string int "title defaults to (untitled)" <|
+        \url size ->
+            [ ( "url", Encode.string url )
+            , ( "size", Encode.int size )
+            ]
+                |> Encode.object
+                |> decodeValue photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
