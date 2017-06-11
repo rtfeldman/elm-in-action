000ef742f588d829bb26d655257ed4bc7467b1dc
Richard Feldman <richard.t.feldman@gmail.com>
2017-06-11 20:17:58 +0200

    Update for 6.3.1

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -19,3 +19,32 @@ decoderTest =
                 |> decodeValue photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
+
+
+stateTransitions : Test
+stateTransitions =
+    describe "state transitions"
+        [ fuzz string "SelectByUrl selects the given photo by URL" <|
+            \url ->
+                PhotoGroove.initialModel
+                    |> PhotoGroove.update (SelectByUrl url)
+                    |> Tuple.first
+                    |> .selectedUrl
+                    |> Expect.equal (Just url)
+        , fuzz (list string) "LoadPhotos selects the first photo" <|
+            \urls ->
+                let
+                    photos =
+                        List.map photoFromUrl urls
+                in
+                    PhotoGroove.initialModel
+                        |> PhotoGroove.update (LoadPhotos (Ok photos))
+                        |> Tuple.first
+                        |> .selectedUrl
+                        |> Expect.equal (List.head urls)
+        ]
+
+
+photoFromUrl : String -> Photo
+photoFromUrl url =
+    { url = url, size = 0, title = "" }
