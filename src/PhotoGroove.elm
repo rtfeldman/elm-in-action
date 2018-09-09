8a2b84e32042e2e44e6f349697696c3a9653e4c6
Richard Feldman <richard.t.feldman@gmail.com>
2018-09-09 13:14:22 -0400

    End of 3.1.2

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,5 +1,6 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -42,6 +43,11 @@ initialModel =
         ]
     , selectedUrl = "1.jpeg"
     }
+
+
+photoArray : Array { url : String }
+photoArray =
+    Array.fromList initialModel.photos
 
 
 update msg model =
