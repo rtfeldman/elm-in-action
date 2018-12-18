30579c88ba33f0efc421504409da494374c7d4cd
Richard Feldman <richard.t.feldman@gmail.com>
2018-12-18 15:59:37 -0300

    End of 6.3.1

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,4 +1,4 @@
-port module PhotoGroove exposing (Model, Msg(..), Photo, Status(..), initialModel, main, photoDecoder, update)
+port module PhotoGroove exposing (Model, Msg(..), Photo, Status(..), initialModel, main, photoDecoder, update, urlPrefix, view)
 
 import Browser
 import Html exposing (..)
