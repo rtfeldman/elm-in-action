4b70fa0a52179d0a32dc91857a42b2394c18ab7c
Richard Feldman <oss@rtfeldman.com>
2019-10-08 21:18:14 -0400

    End of 7.2.2

--- src/PhotoFolders.elm
+++ src/PhotoFolders.elm
@@ -15,6 +15,7 @@ type Folder
         { name : String
         , photoUrls : List String
         , subfolders : List Folder
+        , expanded : Bool
         }
 
 
@@ -29,7 +30,7 @@ initialModel : Model
 initialModel =
     { selectedPhotoUrl = Nothing
     , photos = Dict.empty
-    , root = Folder { name = "Loading...", photoUrls = [], subfolders = [] }
+    , root = Folder { name = "Loading...", expanded = True, photoUrls = [], subfolders = [] }
     }
 
 
@@ -56,22 +57,25 @@ modelDecoder =
         , root =
             Folder
                 { name = "Photos"
+                , expanded = True
                 , photoUrls = []
                 , subfolders =
                     [ Folder
                         { name = "2016"
+                        , expanded = True
                         , photoUrls = [ "trevi", "coli" ]
                         , subfolders =
-                            [ Folder { name = "outdoors", photoUrls = [], subfolders = [] }
-                            , Folder { name = "indoors", photoUrls = [ "fresco" ], subfolders = [] }
+                            [ Folder { name = "outdoors", expanded = True, photoUrls = [], subfolders = [] }
+                            , Folder { name = "indoors", expanded = True, photoUrls = [ "fresco" ], subfolders = [] }
                             ]
                         }
                     , Folder
                         { name = "2017"
+                        , expanded = True
                         , photoUrls = []
                         , subfolders =
-                            [ Folder { name = "outdoors", photoUrls = [], subfolders = [] }
-                            , Folder { name = "indoors", photoUrls = [], subfolders = [] }
+                            [ Folder { name = "outdoors", expanded = True, photoUrls = [], subfolders = [] }
+                            , Folder { name = "indoors", expanded = True, photoUrls = [], subfolders = [] }
                             ]
                         }
                     ]
@@ -82,11 +86,15 @@ modelDecoder =
 type Msg
     = ClickedPhoto String
     | GotInitialModel (Result Http.Error Model)
+    | ClickedFolder FolderPath
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        ClickedFolder path ->
+            ( { model | root = toggleExpanded path model.root }, Cmd.none )
+
         ClickedPhoto url ->
             ( { model | selectedPhotoUrl = Just url }, Cmd.none )
 
@@ -178,3 +186,31 @@ viewFolder (Folder folder) =
 urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
+
+
+type FolderPath
+    = End
+    | Subfolder Int FolderPath
+
+
+toggleExpanded : FolderPath -> Folder -> Folder
+toggleExpanded path (Folder folder) =
+    case path of
+        End ->
+            Folder { folder | expanded = not folder.expanded }
+
+        Subfolder targetIndex remainingPath ->
+            let
+                subfolders : List Folder
+                subfolders =
+                    List.indexedMap transform folder.subfolders
+
+                transform : Int -> Folder -> Folder
+                transform currentIndex currentSubfolder =
+                    if currentIndex == targetIndex then
+                        toggleExpanded remainingPath currentSubfolder
+
+                    else
+                        currentSubfolder
+            in
+            Folder { folder | subfolders = subfolders }
