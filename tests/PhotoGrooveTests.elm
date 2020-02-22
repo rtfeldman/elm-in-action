542fd4a9f41f10010d2adc4ce7e727c65f5db9ed
Richard Feldman <oss@rtfeldman.com>
2020-02-22 18:51:17 -0500

    End of 6.2.3

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -30,3 +30,23 @@ slidHueSetsHue =
                 |> Tuple.first
                 |> .hue
                 |> Expect.equal amount
+
+
+sliders : Test
+sliders =
+    describe "Slider sets the desired field in the Model"
+        [ testSlider "SlidHue" SlidHue .hue
+        , testSlider "SlidRipple" SlidRipple .ripple
+        , testSlider "SlidNoise" SlidNoise .noise
+        ]
+
+
+testSlider : String -> (Int -> Msg) -> (Model -> Int) -> Test
+testSlider description toMsg amountFromModel =
+    fuzz int description <|
+        \amount ->
+            initialModel
+                |> update (toMsg amount)
+                |> Tuple.first
+                |> amountFromModel
+                |> Expect.equal amount
